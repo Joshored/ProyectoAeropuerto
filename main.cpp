1f9b04ae2d7ea76d3ff6260a7e648200c81652a5
Joshored <jorgelpr.lh@gmail.com>
2025-10-22 15:51:39 -0600

    mensaje

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
-    std::cout << "Hola Fer" << lang << "!\n";
+    std::cout << "Hola Ferh" << lang << "!\n";
 
     for (int i = 1; i <= 5; i++)
     {
